dedffa610f1691aa2d5a156d02abcbc1c98ac5c8
Daniel Fang <danielfang7@gmail.com>
2024-10-22 23:22:02 -0700

    Array

--- today-i-learned/script.js
+++ today-i-learned/script.js
@@ -9,3 +9,14 @@ btnOpen.addEventListener(`click`, () => {
     ? "Share a fact"
     : "Close";
 });
+
+const calcFactAge2 = (year) =>
+  year <= new Date().getFullYear()
+    ? new Date().getFullYear() - year
+    : `Impossible year. Year needs to be less or equal to ${new Date().getFullYear()}`;
+console.log(calcFactAge2(1997)); // returns 27
+console.log(calcFactAge2(2026)); // returns `Impossible year. Year needs to be less or equal to 2024`
+
+const fact = ["Fact 1", 2015, true];
+const newFact = [...fact, "society"];
+console.log(newFact); // logs a new array with ["Fact 1", 2015, true, "society"]
