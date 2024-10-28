b3a14408041f040dfe7ca867f35bc57cbe304306
Daniel Fang <daniel@gladetool.com>
2024-10-28 02:07:37 -0700

    For Each and Map

--- today-i-learned/script.js
+++ today-i-learned/script.js
@@ -1,5 +1,3 @@
-console.log(`Hello World`);
-
 const btnOpen = document.querySelector(`.btn-open`);
 const form = document.querySelector(`.fact-form`);
 
@@ -14,9 +12,85 @@ const calcFactAge2 = (year) =>
   year <= new Date().getFullYear()
     ? new Date().getFullYear() - year
     : `Impossible year. Year needs to be less or equal to ${new Date().getFullYear()}`;
-console.log(calcFactAge2(1997)); // returns 27
-console.log(calcFactAge2(2026)); // returns `Impossible year. Year needs to be less or equal to 2024`
+// console.log(calcFactAge2(1997)); // returns 27
+// console.log(calcFactAge2(2026)); // returns `Impossible year. Year needs to be less or equal to 2024`
 
 const fact = ["Fact 1", 2015, true];
 const newFact = [...fact, "society"];
-console.log(newFact); // logs a new array with ["Fact 1", 2015, true, "society"]
+// console.log(newFact); // logs a new array with ["Fact 1", 2015, true, "society"]
+
+// Creating an Object with a method
+const factObj = {
+  text: "Lisbon is the capital of Portugal",
+  category: "society",
+  createdIn: 2015,
+  isCorrect: true,
+  createSummary: function () {
+    return `The fact "${
+      this.text
+    }" is from the category ${this.category.toUpperCase()}`;
+  },
+};
+
+// Destructuring objects
+const { category, isCorrect } = factObj;
+
+// For each method
+[2, 4, 6, 8].forEach(function (el) {
+  console.log(el);
+});
+
+// If we want a new array: use Map
+const times10 = [2, 4, 6, 8].map((el) => el * 10);
+console.log(times10);
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allCategories = CATEGORIES.map((el) => el.name);
+console.log(allCategories);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const factAges = initialFacts.map((el) => calcFactAge2(el.createdIn));
+console.log(factAges);
