7980f7708a4b27e1bdcac6cac8b5dde6ce4b3f7e
Daniel Fang <danielfang7@gmail.com>
2024-11-19 02:45:10 -0800

    loading component and error handling

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -62,11 +62,21 @@ function Counter() {
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function getFacts() {
-      const { data: facts, error } = await supabase.from("facts").select("*");
-      setFacts(facts);
+      setIsLoading(true);
+      const { data: facts, error } = await supabase
+        .from("facts")
+        .select("*")
+        .order(`votesInteresting`, { ascending: false })
+        .limit(1000);
+
+      if (!error) setFacts(facts);
+      else alert(`There was a problem getting data`);
+
+      setIsLoading(false);
     }
     getFacts();
   }, []);
@@ -80,10 +90,15 @@ function App() {
 
       <main className="main">
         <CategoryFilter />
-        <FactList facts={facts} />
+
+        {isLoading ? <Loader /> : <FactList facts={facts} />}
       </main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="message">Loading...</p>;
 }
 
 function Header({ showForm, setShowForm }) {
