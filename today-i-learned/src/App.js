b7d73837701c55f1845a689b3ec8237474e48e52
Daniel Fang <danielfang7@gmail.com>
2024-11-22 03:12:09 -0800

    Adding facts to Supabase

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -150,28 +150,26 @@ function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
   const [source, setSource] = useState("https://example.com");
   const [category, setCategory] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     // 1. Prevent Browser Reload
     e.preventDefault();
     console.log(text, source, category);
 
     // 2. Check if data is valid. If so, create a new Fact
     if (text && isValidHttpUrl(source) && category && textLength <= 200) {
-      // 3. Create a new Fact Object
-      const newFact = {
-        id: Math.round(Math.random() * 1000000),
-        text,
-        source,
-        category,
-        votesInteresting: 0,
-        votesMindblowing: 0,
-        votesFalse: 0,
-        createdIn: new Date().getFullYear(),
-      };
+      // 3. Upload fact to Supabase and receive the new fact object (only passing data that isn't auto created)
+      setIsUploading(true);
+      const { data: newFact, error } = await supabase
+        .from("facts")
+        .insert([{ text, source, category }])
+        .select();
+      setIsUploading(false);
+
       // 4. Add the new fact to the UI: add the fact to state
-      setFacts((facts) => [newFact, ...facts]);
+      setFacts((facts) => [newFact[0], ...facts]);
     }
 
     // 5. Reset input fields
@@ -197,8 +195,13 @@ function NewFactForm({ setFacts, setShowForm }) {
         placeholder="Input your trustworthy source..."
         value={source}
         onChange={(e) => setSource(e.target.value)}
+        disabled={isUploading}
       />
-      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+      <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        disabled={isUploading}
+      >
         <option value="">Choose category:</option>
         {CATEGORIES.map((cat) => (
           <option key={cat.name} value={cat.name}>
@@ -206,7 +209,9 @@ function NewFactForm({ setFacts, setShowForm }) {
           </option>
         ))}
       </select>
-      <button className="btn btn-large">Post</button>
+      <button className="btn btn-large" disabled={isUploading}>
+        Post
+      </button>
     </form>
   );
 }
